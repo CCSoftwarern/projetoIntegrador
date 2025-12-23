543de3ef2e4a17d7d9933227ee00536eaec38dda
Claudemyr <claudemyr.medeiros@escolar.ifrn.edu.br>
2025-12-23 10:59:56 -0300

    Conexão com Postgres

--- backend/projSysInventory/settings.py
+++ backend/projSysInventory/settings.py
@@ -74,12 +74,27 @@ WSGI_APPLICATION = 'projSysInventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbSysInventory',
+        'USER': 'postgres',
+        'PASSWORD': 'masterkey',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        },
     }
 }
+
 
 
 # Password validation
